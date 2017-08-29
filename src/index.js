fc14c73466d38261004c190730823ef869f63005
Dmitriy Khraponov <snupt@snupt.com>
2017-08-29 11:53:50 +0300

    step #2, fix function naming

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const makeWelcome = () => {
+const makeWelcome = () => {
   console.log('Welcome to the Brain Games!\n');
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
 };
+
+export default makeWelcome;
