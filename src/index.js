8fcd37e0bbef24542cdb7bb03aa7f4ec443d758f
Dmitriy Khraponov <snupt@snupt.com>
2017-08-29 09:31:40 +0300

    step #3, add code quality services

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 import readlineSync from 'readline-sync';
 
 const userName = () => {
-	console.log('Welcome to the Brain Games!\n');
-	const actual = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${actual}!`);
+  console.log('Welcome to the Brain Games!\n');
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actual}!`);
 };
 
 export default userName;
