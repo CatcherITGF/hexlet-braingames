cba22e92e78760bba64c5ec6063854f546a5126a
Dmitriy Khraponov <snupt@snupt.com>
2017-09-03 14:17:42 +0300

    add new mini game brain-balance

---
+++ src/games/balance.js
@@ -0,0 +1,40 @@
+import { cons } from 'hexlet-pairs';
+import buildGame from '..';
+import makeRandomInt from '../random-integer';
+
+const rules = 'Balance the given number.';
+
+const doBalance = (strNum) => {
+  const strNumLength = strNum.length - 1;
+  const doSort = str => str.split('').sort().join('');
+  const getMin = str => Number(doSort(str)[0]);
+  const getMax = str => Number(doSort(str)[strNumLength]);
+  const isBalance = str => ((getMax(str) - getMin(str)) <= 1) && (doSort(str) === str);
+
+  const incFirstDecLast = (str) => {
+    const incFirst = Number(str[0]) + 1;
+    const middle = str.substring(1, strNumLength);
+    const decLast = Number(str[strNumLength]) - 1;
+    return `${incFirst}${middle}${decLast}`;
+  };
+
+  const getResult = (str) => {
+    if (isBalance(str)) {
+      return str;
+    }
+    return getResult(doSort(incFirstDecLast(str)));
+  };
+
+  return getResult(strNum);
+};
+
+const makeQA = () => {
+  const question = makeRandomInt(100, 9999);
+  const answer = doBalance(String(question));
+
+  return cons(question, answer);
+};
+
+const startGame = () => buildGame(rules, makeQA);
+
+export default startGame;
