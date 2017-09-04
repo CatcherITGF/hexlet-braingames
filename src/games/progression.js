933b729c67c8589088a6b2e7f1ba29a73c903285
Dmitriy Khraponov <snupt@snupt.com>
2017-09-04 14:01:08 +0300

    refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,35 +1,33 @@
-import { cons, car, cdr } from 'hexlet-pairs';
+import { cons } from 'hexlet-pairs';
 import buildGame from '..';
 import makeRandomInt from '../random-integer';
 
 const rules = 'What number is missing in this progression?';
 
-let result1 = '';
-let result2 = '';
+const getN = (n, a, d) => a + ((n - 1) * d);
 
-const getN = (n, a, d) => a + (n - 1) * d;
-
-const makeProgression = (a, d) => {
-  for (let i = 0; i < 10; i += 1) {
+const makeProgression = (a, d, hide) => {
+  let question = '';
+  let answer = '';
+  for (let i = 1; i <= 10; i += 1) {
     const buildString = getN(i, a, d);
-    if (i === 5) {
-      result1 += '.. ';
-      result2 = buildString;
+    if (i === hide) {
+      question += '.. ';
+      answer = buildString;
     } else {
-      result1 += `${buildString} `;
+      question += `${buildString} `;
     }
   }
+  question = question.slice(0, -1);
+  return cons(question, answer);
 };
 
 const makeQA = () => {
   const member = makeRandomInt(1, 10);
   const diff = makeRandomInt(1, 10);
-  const qa = makeProgression(member, diff);
+  const hide = makeRandomInt(3, 8);
 
-  const question = result1;
-  const answer = result2;
-
-  return cons(question, answer);
+  return makeProgression(member, diff, hide);
 };
 
 const startGame = () => buildGame(rules, makeQA);
