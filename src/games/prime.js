ff5a630c65dce4a0e7900a9cd39308b7408d6a95
Dmitriy Khraponov <snupt@snupt.com>
2017-09-04 11:06:49 +0300

    fix function isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,14 +6,15 @@ const rules = 'Answer "yes" if number prime otherwise answer "no".';
 
 const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return 'no';
+    if (num % i === 0) return false;
   }
-  return num !== 1 ? 'yes' : 'no';
+  return num !== 1;
 };
 
 const makeQA = () => {
   const question = makeRandomInt(1, 99);
-  const answer = isPrime(question);
+  const getAnswer = () => isPrime(question) ? 'yes' : 'no';
+  const answer = getAnswer();
 
   return cons(question, answer);
 };
